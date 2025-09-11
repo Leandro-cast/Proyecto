9d783ba2baa59de402a54c7850b15c20d65cfb7e
KyexC <kiollacorbalan@gmail.com>
2025-09-11 17:34:00 -0300

    Summary

---
+++ js/preguntas.js
@@ -0,0 +1,8 @@
+let botones = document.querySelectorAll(".botondeApertura");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", function(){
+        let desplegable = this.nextElementSibling;
+        desplegable.classList.toggle("activo");
+    });
+});
