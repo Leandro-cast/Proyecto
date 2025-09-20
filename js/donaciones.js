8316bed380701b309e2eb6bbf1c0839907b807a3
santiago75castro89 <santiago75castro89@gmail.com>
2025-09-20 19:20:36 -0300

    Update donaciones.js

--- js/donaciones.js
+++ js/donaciones.js
@@ -1,55 +1,92 @@
 
 //Registro Donante
-const nombre = document.getElementById("nombreDonante")
-const apellido = document.getElementById("apellidoDonante")
-const cumpleaños = document.getElementById("cumpleañosDonante")
-const documento = document.getElementById("documentoDonante")
-const telefono = document.getElementById("telefonoDonante")
-const email = document.getElementById("emailDonante")
-const tarjeta = document.getElementById("tarjetaDonante")
-const guardarBtn = document.getElementById("guardarBtn")
+        const nombre = document.getElementById("nombreDonante")
+        const apellido = document.getElementById("apellidoDonante")
+        const cumpleaños = document.getElementById("cumpleañosDonante")
+        const documento = document.getElementById("documentoDonante")
+        const telefono = document.getElementById("telefonoDonante")
+        const email = document.getElementById("emailDonante")
+        const tarjeta = document.getElementById("tarjetaDonante")
+        const guardarBtn = document.getElementById("guardarBtn")
 
-document.getElementById("nombreDonanteFinal").textContent = localStorage.getItem("nombreGuardado") || "indefinido"
-document.getElementById("apellidoDonanteFinal").textContent = localStorage.getItem("apellidoGuardado") || "indefinido"
-document.getElementById("cumpleañosDonanteFinal").textContent = localStorage.getItem("cumpleañosGuardado") || "indefinido"
-document.getElementById("documentoDonanteFinal").textContent = localStorage.getItem("documentoGuardado") || "indefinido"
-document.getElementById("telefonoDonanteFinal").textContent = localStorage.getItem("telefonoGuardado") || "indefinido"
-document.getElementById("emailDonanteFinal").textContent = localStorage.getItem("emailGuardado") || "indefinido"
-document.getElementById("tarjetaDonanteFinal").textContent = localStorage.getItem("tarjetaGuardado") || "indefinido"
+        document.getElementById("nombreDonanteFinal").textContent = localStorage.getItem("nombreGuardado") || "indefinido"
+        document.getElementById("apellidoDonanteFinal").textContent = localStorage.getItem("apellidoGuardado") || "indefinido"
+        document.getElementById("cumpleañosDonanteFinal").textContent = localStorage.getItem("cumpleañosGuardado") || "indefinido"
+        document.getElementById("documentoDonanteFinal").textContent = localStorage.getItem("documentoGuardado") || "indefinido"
+        document.getElementById("telefonoDonanteFinal").textContent = localStorage.getItem("telefonoGuardado") || "indefinido"
+        document.getElementById("emailDonanteFinal").textContent = localStorage.getItem("emailGuardado") || "indefinido"
+        document.getElementById("tarjetaDonanteFinal").textContent = localStorage.getItem("tarjetaGuardado") || "indefinido"
 
-function validarTodo() {
-    if (nombre.value.trim() === "" || apellido.value.trim() === "" || cumpleaños.value.trim() === "" || documento.value.trim() === "" || telefono.value.trim() === "" || email.value.trim() === "" || tarjeta.value.trim() === ""
-    ) {
-        alert("Por favor, completa todos los campos antes de guardar.")
-    return false
-    }
-    return true
-}
+        function validarTodo() {
+            if (nombre.value.trim() === "" || 
+            apellido.value.trim() === "" || 
+            cumpleaños.value.trim() === "" || 
+            documento.value.trim() === "" || 
+            telefono.value.trim() === "" || 
+            email.value.trim() === "" || 
+            tarjeta.value.trim() === "" 
+            ) {
+                alert("Por favor, completa todos los campos antes de guardar.")
+                return false
+            }
+            if (nombre.value.trim().length < 3) {
+                alert("El nombre no es valido.")
+                return false
+            }
+            if (apellido.value.trim().length < 3) {
+                alert("El apellido no es valido.")
+                return false
+            }
+            if (!email.value.includes("@gmail.com")) {
+                alert("El email no es valido.")
+                return false
+            }
+            if (documento.value.trim().length !== 8) {
+                alert("El documento no es valido.")
+                return false
+            }
+            if (telefono.value.trim().length < 8){
+                alert("El numero de telefono no es valido.")
+                return false
+            }
+            if (tarjeta.value.trim().lenght < 14){
+                alert("La tarjeta no es valida.")
+                return false
+            }
 
-guardarBtn.addEventListener("click", () => {
-    if (validarTodo() === false) {
-        alert("Por favor, completa todos los campos antes de guardar.")
-    } else {
-        localStorage.setItem("nombreGuardado", nombre.value)
-        localStorage.setItem("apellidoGuardado", apellido.value)
-        localStorage.setItem("cumpleañosGuardado", cumpleaños.value)
-        localStorage.setItem("documentoGuardado", documento.value)
-        localStorage.setItem("telefonoGuardado", telefono.value)
-        localStorage.setItem("emailGuardado", email.value)
-        localStorage.setItem("tarjetaGuardado", tarjeta.value)
+            return true
+            }
+        guardarBtn.addEventListener("click", () => {
+            if (validarTodo() === false) {
+                alert("Por favor, completa todos los campos antes de guardar.")
+            } else {
+                localStorage.setItem("nombreGuardado", nombre.value)
+                localStorage.setItem("apellidoGuardado", apellido.value)
+                localStorage.setItem("cumpleañosGuardado", cumpleaños.value)
+                localStorage.setItem("documentoGuardado", documento.value)
+                localStorage.setItem("telefonoGuardado", telefono.value)
+                localStorage.setItem("emailGuardado", email.value)
+                localStorage.setItem("tarjetaGuardado", tarjeta.value)
 
-        document.getElementById("nombreDonanteFinal").textContent = nombre.value
-        document.getElementById("apellidoDonanteFinal").textContent = apellido.value
-        document.getElementById("cumpleañosDonanteFinal").textContent = cumpleaños.value
-        document.getElementById("documentoDonanteFinal").textContent = documento.value
-        document.getElementById("telefonoDonanteFinal").textContent = telefono.value
-        document.getElementById("emailDonanteFinal").textContent = email.value
-        document.getElementById("tarjetaDonanteFinal").textContent = tarjeta.value
+                document.getElementById("nombreDonanteFinal").textContent = nombre.value
+                document.getElementById("apellidoDonanteFinal").textContent = apellido.value
+                document.getElementById("cumpleañosDonanteFinal").textContent = cumpleaños.value
+                document.getElementById("documentoDonanteFinal").textContent = documento.value
+                document.getElementById("telefonoDonanteFinal").textContent = telefono.value
+                document.getElementById("emailDonanteFinal").textContent = email.value
+                document.getElementById("tarjetaDonanteFinal").textContent = tarjeta.value
 
-        alert("registro guardado con éxito.")
-    }
-})
+                alert("registro guardado con éxito. Para finalizar con la donacion, precione el boton Enviar Donaciones")
 
+                if (!document.getElementById("enlaceExito")) {
+                const link = document.createElement("a")
+                link.id = "enlaceExito"
+                link.textContent = "Enviar Donaciones"
+                link.href = "pages/EnviarDonaciones.html"
+                document.getElementById("InfoUsuario").appendChild(link)
+                }
+            }
+        })
 //Enviar Donaciones
 const boton = document.getElementById("botonDonar")
 boton.addEventListener("click", () => {
